2a482c7e4d6db4dcacfd241dad296366a24f9c29
huajiaoshu520 <huajiaoshu@jasonml.eu.org>
2026-08-01 22:21:53 +0800

    Update diy-part1.sh

--- diy_script/diy-part1.sh
+++ diy_script/diy-part1.sh
@@ -17,8 +17,8 @@ sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(LINUX_VERSION)-/g' include/image.mk
 
 # Add a feed source
 #sed -i 's/23\.05/25.12/g' feeds.conf.default
-echo "src-git helloworld https://github.com/Jason6111/helloworld;dev" >> "feeds.conf.default"
-#echo "src-git helloworld https://github.com/fw876/helloworld;dev" >> "feeds.conf.default"
+#echo "src-git helloworld https://github.com/Jason6111/helloworld;dev" >> "feeds.conf.default"
+echo "src-git helloworld https://github.com/fw876/helloworld;dev" >> "feeds.conf.default"
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 #echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
