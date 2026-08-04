2fb4732836b4c0bf4fc1e5d4ee60192acb46e846
huajiaoshu520 <huajiaoshu@jasonml.eu.org>
2026-08-05 05:53:03 +0800

    Update diy-part1.sh

--- diy_script/diy-part1.sh
+++ diy_script/diy-part1.sh
@@ -23,6 +23,7 @@ echo "src-git helloworld https://github.com/fw876/helloworld;dev" >> "feeds.conf
 #echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 #echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+echo "src-git daed https://github.com/QiuSimons/luci-app-daed" >> "feeds.conf.default"
 
 # Temp
 #rm -rf ./target/linux/generic/hack-6.12/220-arm-gc_sections.patch
