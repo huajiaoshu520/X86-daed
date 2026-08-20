22514cfb3da7bd5b732d83ba9156e1e272b13f36
huajiaoshu520 <huajiaoshu@jasonml.eu.org>
2026-08-20 10:24:48 +0800

    Update diy-part1.sh

--- diy_script/diy-part1.sh
+++ diy_script/diy-part1.sh
@@ -31,4 +31,3 @@ cp ./include/kernel-6.18 ./target/linux/generic
 rm -rf package/kernel/bpf-headers
 git clone https://github.com/huajiaoshu520/bpf-headers ./package/kernel/bpf-headers
 
-sed -i 's/DEFAULT:=y if IPTABLES_CONNLABEL/DEFAULT:=n/' package/network/utils/iptables/Makefile
