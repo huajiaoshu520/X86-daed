6e62bb7a7a3c9ca47cb3b86e6a28b59126083ae5
huajiaoshu520 <huajiaoshu@jasonml.eu.org>
2026-08-20 15:06:18 +0800

    Update diy-part2.sh

--- diy_script/diy-part2.sh
+++ diy_script/diy-part2.sh
@@ -48,3 +48,4 @@ sed -i '/^\[ -n "\$\${IPKG_INSTROOT}" \] \|\| echo "0 4 \* \* \* \/etc\/coremark
 #同步lede-xray
 cp ./feeds/helloworld/xray-core/Makefile ./feeds/packages/net/xray-core/Makefile
 
+rm -rf ./feeds/package/network/utils/iptables
