768880df1414182037ea0a89b6e1d628b498d82b
huajiaoshu520 <huajiaoshu@jasonml.eu.org>
2026-09-02 18:03:29 +0800

    Update diy-part1.sh

--- diy_script/diy-part1.sh
+++ diy_script/diy-part1.sh
@@ -23,8 +23,8 @@ echo "src-git daed https://github.com/kenzok8/openwrt-daede" >> "feeds.conf.defa
 echo "src-git quickstart https://github.com/huajiaoshu520/quickstart" >> "feeds.conf.default"
 
 #daed
-#curl -L https://raw.githubusercontent.com/huajiaoshu520/X86-daed/refs/heads/main/daed/patches/llvm-bpf/Makefile -o tools/llvm-bpf/Makefile
-#curl -L https://raw.githubusercontent.com/huajiaoshu520/X86-daed/refs/heads/main/daed/patches/bpf.mk -o include/bpf.mk
+#curl -L https://raw.githubusercontent.com/huajiaoshu520/X86-daed/refs/heads/main/patches/daed/patches/llvm-bpf/Makefile -o tools/llvm-bpf/Makefile
+#curl -L https://raw.githubusercontent.com/huajiaoshu520/X86-daed/refs/heads/main/patches/daed/patches/bpf.mk -o include/bpf.mk
 
 #修改daed编译内核
 cp ./include/kernel-6.18 ./target/linux/x86/generic
