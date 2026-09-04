6e8fc4eaf76a4fdb084ddfe65afd8016eda384ac
huajiaoshu520 <huajiaoshu@jasonml.eu.org>
2026-09-04 10:33:24 +0800

    Update diy-docker.sh

--- diy_script/diy-docker.sh
+++ diy_script/diy-docker.sh
@@ -36,8 +36,8 @@ sed -i -e '\|$(call EnsureVendoredVersion,containerd)|{s/^/# /}' \
        -e '\|$(call EnsureVendoredVersion,runc)|{s/^/# /}' \
        ./feeds/packages/utils/dockerd/Makefile
 # 补丁      
-mkdir -p ./feeds/packages/utils/dockerd/patches
-wget -O ./feeds/packages/utils/dockerd/patches/001-skip-copy-nested-binaries.patch \
+#mkdir -p ./feeds/packages/utils/dockerd/patches
+#wget -O ./feeds/packages/utils/dockerd/patches/001-skip-copy-nested-binaries.patch \
   https://raw.githubusercontent.com/huajiaoshu520/X86-daed/refs/heads/main/patches/dockerd/patches/001-skip-copy-nested-binaries.patch
 
 # fw4 docker
