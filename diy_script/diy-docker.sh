15e59e2d7c5905f3b378b4c5200577c7899bd97f
huajiaoshu520 <huajiaoshu@jasonml.eu.org>
2026-09-05 05:10:26 +0800

    Update diy-docker.sh

--- diy_script/diy-docker.sh
+++ diy_script/diy-docker.sh
@@ -21,15 +21,15 @@
 # dockerd
 # wget https://codeload.github.com/moby/moby/tar.gz/docker-v29.7.2
 # sha256sum docker-v29.7.2
-sed -i -e 's/29.6.1/29.8.0/g' \
-       -e 's/a97bd870c4b072b7d9cc053b2a806ca3d920f192f9dc6a662e17c1b69f56f2e1/e75ffb5d2ddc1fd98138fdb5e29f707b59f415ec8697e73b8bbdf8bbbb4be8eb/g' \
-       -e 's/8ec5ab3/3ce5872/g' ./feeds/packages/utils/dockerd/Makefile
+sed -i -e 's/29.6.1/29.7.2/g' \
+       -e 's/a97bd870c4b072b7d9cc053b2a806ca3d920f192f9dc6a662e17c1b69f56f2e1/3a93a88bff41ffa6f4dca9f4ed9fc05e7fdb08e0f9014cf1d8177f85ecbc0683/g' \
+       -e 's/8ec5ab3/6a43e3d/g' ./feeds/packages/utils/dockerd/Makefile
 
 # docker
 # wget https://codeload.github.com/docker/cli/tar.gz/v29.7.2
-sed -i -e 's/29.6.1/29.8.0/g' \
-       -e 's/74d14dd212b07cd3328989dc6a029dde2ebbe6a878199eaaafad54916f456194/c5fadbc00c02dbecb1b7c9936e188baf9c80421a9107e7e9ad36a0923a0fc764/g' \
-       -e 's/8900f1d/88096ef/g' ./feeds/packages/utils/docker/Makefile
+sed -i -e 's/29.6.1/29.7.2/g' \
+       -e 's/74d14dd212b07cd3328989dc6a029dde2ebbe6a878199eaaafad54916f456194/225b7ab2a15f5230b482df8461069cd4bce38891266fb9898d4188d0a3cbf54a/g' \
+       -e 's/8900f1d/a7dcaa6/g' ./feeds/packages/utils/docker/Makefile
 
 # 禁用
 sed -i -e '\|$(call EnsureVendoredVersion,containerd)|{s/^/# /}' \
@@ -39,8 +39,7 @@ sed -i -e '\|$(call EnsureVendoredVersion,containerd)|{s/^/# /}' \
 mkdir -p ./feeds/packages/utils/dockerd/patches
 wget -O ./feeds/packages/utils/dockerd/patches/001-skip-copy-nested-binaries.patch \
   https://raw.githubusercontent.com/huajiaoshu520/X86-daed/refs/heads/main/patches/dockerd/patches/001-skip-copy-nested-binaries.patch
-wget -O ./feeds/packages/utils/docker/Makefile \
-  https://raw.githubusercontent.com/huajiaoshu520/X86-daed/refs/heads/main/patches/docker/Makefile
+
 # fw4 docker
 mkdir -p package/base-files/files/etc/docker
 
